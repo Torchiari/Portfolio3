3ff70689f46074b919ebbfd60a3173da55f80cf7
Torchiari <danel.t@hotmail.com>
2025-06-18 16:56:39 -0300

    pusheado

--- portfolio-danel/src/components/Hero.jsx
+++ portfolio-danel/src/components/Hero.jsx
@@ -2,15 +2,41 @@ const Hero = () => {
     return (
         <section
             id="home"
-            className="">
+            className="pt-28 lg:pt-36">
             <div className="">
 
                 <div>
 
                     <div className="">
-                        figure
+                        <figure className="">
+                            <img src="/images/avatar-1.jpg" width={40} height={40} alt="Danel Torchiari portrait" className="img-cover" />
+                        </figure>
+
+                        <div className="">
+                            <span className="">
+                                <span className=""></span>
+                            </span>
+
+                            Available for work
+                        </div>
                     </div>
 
+                    <h2 className="">
+                        Building Scalable Modern Websites for the Future
+                    </h2>
+
+                    <div className="">
+                        ButtonPrimary
+
+                        ButtonOutline
+                    </div>
+
+                </div>
+
+                <div className="">
+                    <figure className="">
+                        <img src="/images/hero-banner.png" width={656} height={800} alt="Danel Torchiari" className="w-full" />
+                    </figure>
                 </div>
 
 
